6784a7595278fbaa87294a0ca2cbe337ecf963d7
Arti <arti.chourey21@gmail.com>
2026-02-11 22:19:19 +0530

    feat: add product-service global exception handling

---
+++ src/main/java/com/artichourey/ecommerce/productservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.artichourey.ecommerce.productservice.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	public ResourceNotFoundException() {
+		super();
+		
+	}
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+		
+	}
+	
+	
+
+}
