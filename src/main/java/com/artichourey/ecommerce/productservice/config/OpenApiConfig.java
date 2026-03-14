6a3fd412c1bb82cb675aae7ced9dcf0eddea18c3
Arti <arti.chourey21@gmail.com>
2026-03-14 21:25:15 +0530

    chore(config): add Swagger/OpenAPI configuration bean

---
+++ src/main/java/com/artichourey/ecommerce/productservice/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.artichourey.ecommerce.productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+	@Bean
+	public OpenAPI productServiceAPI() {
+	    return new OpenAPI()
+	            .info(new Info()
+	                    .title("Product Service API")
+	                    .description("Product catalog management APIs")
+	                    .version("1.0"));
+	}
+
+}
