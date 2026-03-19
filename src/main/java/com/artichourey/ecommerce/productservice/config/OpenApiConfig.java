5fbd428b674706a2681b36901173944e38821459
Arti <arti.chourey21@gmail.com>
2026-03-19 21:01:41 +0530

    chore(config):add Swagger/OpenApi configuration bean

--- src/main/java/com/artichourey/ecommerce/productservice/config/OpenApiConfig.java
+++ src/main/java/com/artichourey/ecommerce/productservice/config/OpenApiConfig.java
@@ -1,20 +1,47 @@
 package com.artichourey.ecommerce.productservice.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
-	@Bean
-	public OpenAPI productServiceAPI() {
-	    return new OpenAPI()
-	            .info(new Info()
-	                    .title("Product Service API")
-	                    .description("Product catalog management APIs")
-	                    .version("1.0"));
-	}
+	// Product APIs
+    @Bean
+    public GroupedOpenApi productApi() {
+        return GroupedOpenApi.builder()
+                .group("products")
+                .pathsToMatch("/api/products/**")
+                .build();
+    }
 
+    @Bean
+    public GroupedOpenApi categoryApi() {
+        return GroupedOpenApi.builder()
+                .group("categories")
+                .pathsToMatch("/api/categories/**")
+                .build();
+    }
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
 }
+
+
