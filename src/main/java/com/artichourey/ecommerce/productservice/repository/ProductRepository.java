deff1cd97bd9f89b12fc6d9b511f0605c8d8c59f
Arti <arti.chourey21@gmail.com>
2026-04-05 15:08:05 +0530

    refactor: delete old commented queries

--- src/main/java/com/artichourey/ecommerce/productservice/repository/ProductRepository.java
+++ src/main/java/com/artichourey/ecommerce/productservice/repository/ProductRepository.java
@@ -22,27 +22,11 @@ public interface ProductRepository extends JpaRepository <Product , Long>{
 	
    //JPQL Queries
 	
-	/*@Query("SELECT p FROM Product p"+
-	       "WHERE LOWER(p.name) LIKE LOWER(CONCAT('%',:keyword,'%')"+
-			"OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword,'%'))")
-	Page<Product> searchProducts(@Param("keyword")String keyword,Pageable pageable);
-	*/
-	
 	@Query("SELECT p FROM Product p " +
 		       "WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
 		       "OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
 		Page<Product> searchProducts(@Param("keyword") String keyword, Pageable pageable);
 
-	/*@Query("SELECT p FROM Product p"+
-			 "WHERE(:keyword IS NULL OR LOWER(p.name))LIKE LOWER(CONCAT('%',:keyword,'%'))"+
-			"AND(:categoryId IS NULL OR p.category.id=:categoryId)"+
-			 "AND(p.price BETWEEN :minPrice AND :maxPrice)")
-	Page<Product> advanceFilter(
-			@Param("keyword") String keyword,
-			@Param("categoryId") Long categoryId,
-			@Param("minPrice")Double minPrice,
-			@Param("maxPrice")Double maxPrice, Pageable pageable); */
-	
 	
 	@Query("SELECT p FROM Product p " +
 		       "WHERE (:keyword IS NULL OR " +
