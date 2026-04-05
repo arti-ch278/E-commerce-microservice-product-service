2941717e26fb93f0e770f0308b6fc28016ef5b58
Arti <arti.chourey21@gmail.com>
2026-04-05 15:06:15 +0530

    feat(product): add skuCode and improve price precision using BigDecimal

--- src/main/java/com/artichourey/ecommerce/productservice/mapper/ProductMapper.java
+++ src/main/java/com/artichourey/ecommerce/productservice/mapper/ProductMapper.java
@@ -5,22 +5,37 @@ import org.springframework.stereotype.Component;
 import com.artichourey.ecommerce.productservice.dtos.ProductDto;
 import com.artichourey.ecommerce.productservice.entity.Category;
 import com.artichourey.ecommerce.productservice.entity.Product;
+
 @Component
 public class ProductMapper {
-	
-	public ProductDto toDto(Product product) {
-		return ProductDto.builder().id(product.getId()).name(product.getName()).
-				brand(product.getBrand()).description(product.getDescription())
-				.imageUrl(product.getImageUrl()).price(product.getPrice())
-				.discountPrice(product.getDiscountPrice()).quantity(product.getQuantity())
-				.categoryId(product.getCategory() !=null ? product.getCategory().getId() : null).build();
-		
-		
-	}
-	public Product toEntity(ProductDto dto, Category category) {
-		return Product.builder().id(dto.getId()).name(dto.getName()).brand(dto.getBrand())
-				.description(dto.getDescription()).price(dto.getPrice()).discountPrice(dto.getDiscountPrice())
-				.imageUrl(dto.getImageUrl()).quantity(dto.getQuantity()).category(category).build();
-	}
 
+    public ProductDto toDto(Product product) {
+        return ProductDto.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .brand(product.getBrand())
+                .description(product.getDescription())
+                .imageUrl(product.getImageUrl())
+                .price(product.getPrice())
+                .discountPrice(product.getDiscountPrice())
+                .quantity(product.getQuantity())
+                .skuCode(product.getSkuCode())
+                .categoryId(product.getCategory() != null ? product.getCategory().getId() : null)
+                .build();
+    }
+
+    public Product toEntity(ProductDto dto, Category category) {
+        return Product.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .brand(dto.getBrand())
+                .description(dto.getDescription())
+                .price(dto.getPrice())
+                .discountPrice(dto.getDiscountPrice())
+                .imageUrl(dto.getImageUrl())
+                .quantity(dto.getQuantity())
+                .skuCode(dto.getSkuCode()) 
+                .category(category)
+                .build();
+    }
 }
